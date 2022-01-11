4f84eab8ec0bef032a96382c11cdb2ea63f9af91
Samaneh Aria <samaneharia.75@gmail.com>
2022-01-11 17:56:33 +0330

    checking if dockerservice exists or not

--- docker-installation/installDocker.sh
+++ docker-installation/installDocker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-set -e
-
 if [ $(id -u) != 0 ]; then
   echo -e "It must be root user."
   exit 1
+fi 
+
+check=$(systemctl --type=service | grep docker.service)
+status=$? 
+
+if [ $status -eq 1 ];then
+  echo "This service doesnt exist"
+else
+  echo "This servis exist"
 fi
