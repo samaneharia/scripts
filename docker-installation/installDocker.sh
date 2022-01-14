d190d7b36307d7b36c8cc21af11c4507e5562729
Samaneh Aria <samaneharia.75@gmail.com>
2022-01-14 16:09:06 +0330

    Installation codes added to the script

--- docker-installation/installDocker.sh
+++ docker-installation/installDocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 if [ $(id -u) != 0 ]; then
   echo -e "It must be root user."
   exit 1
@@ -9,7 +11,50 @@ check=$(systemctl --type=service | grep docker.service)
 status=$? 
 
 if [ $status -eq 1 ];then
-  echo "This service doesnt exist"
+  
+  #This service doesnt exist.installing the last version.
+  apt-get update
+  apt-get install docker-ce docker-ce-cli containerd.io
+  apt-cache madison docker-ce
+  apt-get install docker-ce=<VERSION_STRING> \ 
+	  docker-ce-cli=<VERSION_STRING> containerd.io
+
+  #installing docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/1.29.2/ \ 
+	  docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+
+  #creating a symbolic link if docker-compose command fails.
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  
+  #checking versions
+  docker --version
+  docker-compose --version
+
 else
-  echo "This servis exist"
+
+  #This servis exist.Removing the existed version.
+  apt-get remove docker docker-engine docker.io containerd runc
+  rm -rf /var/lib/docker
+  rm -rf /var/lib/containerd
+
+  #installing last version.
+  apt-get update                                                               
+  apt-get install docker-ce docker-ce-cli containerd.io                        
+  apt-cache madison docker-ce                                                  
+  apt-get install docker-ce=<VERSION_STRING> \                                 
+        docker-ce-cli=<VERSION_STRING> containerd.io                             
+  
+  #installing docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/1.29.2/ \
+	  docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+ 
+  #creating a symbolic link if docker-compose command fails.
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ 
+  #checking versions
+  docker --version
+  docker-compose --version
+
 fi
